9f4b18c157099596c2398f9e3930d86a200af8d5
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-09-14 01:27:00 +0530

    Added pre-commit configurations, and home page

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,28 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    date_of_birth = models.DateField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
+    fitness_goals = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to="profile/", null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
