8c275dd4fc9d80968f2200129af868d8665c1687
2100032578cse <uwizeyesandri@gmail.com>
2024-09-16 20:55:57 +0530

    dashboard modification

--- pages/views.py
+++ pages/views.py
@@ -8,3 +8,14 @@ class HomePageView(TemplateView):
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "pages/dashboard.html"
+class daily_activitiesView(TemplateView):
+    template_name = "pages/daily_activities.html"
+
+class MoodTrackerView(TemplateView):
+    template_name = "pages/mood_tracker.html"
+
+class ProfileView(TemplateView):
+    template_name = "pages/profile.html"
+
+class ProgressAnalyticsView(TemplateView):
+    template_name = "pages/progress.html"
