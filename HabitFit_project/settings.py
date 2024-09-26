fffd51946dcc2f250404d7a71f3615712219a7de
2100032578cse <uwizeyesandri@gmail.com>
2024-09-26 16:09:39 +0530

    setting test envronment

--- HabitFit_project/settings.py
+++ HabitFit_project/settings.py
@@ -151,3 +151,6 @@ CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
+
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
